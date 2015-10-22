f931ac6a2b0fe8f94ed8d1612abaacc9c4c4188a
Martin Espinal <marlex.espinal@gmail.com>
2015-10-21 20:48:18 -0600

    Index message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Leaderboard API', msg: 'A NODE.JS Homework' });
 });
 
 module.exports = router;
